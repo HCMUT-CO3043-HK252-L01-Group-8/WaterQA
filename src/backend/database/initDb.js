f2dd6f906b35a5479e4edc75d214d497b549d33c
Lương Gia Minh <luonggminh05@gmail.com>
2026-03-23 21:34:34 +0700

    Update initDb.js

--- src/backend/database/initDb.js
+++ src/backend/database/initDb.js
@@ -2,11 +2,16 @@
 const db = require('./db');
 
 db.exec(`
-  CREATE TABLE IF NOT EXISTS Accounts (
-    phone VARCHAR(12),
-    hashedPass VARCHAR(255),
-    PRIMARY KEY (phone)
-  )
+  CREATE TABLE IF NOT EXISTS USER (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT,
+    phone_number TEXT UNIQUE,
+    password_hash TEXT,
+    role TEXT,
+    verification_status INTEGER,
+    created_at TEXT,
+    updated_at TEXT
+  );
 `);
 
-console.log('Table Accounts is ready');
+console.log('Table USER is ready');
