5c3faf131f678d0b07977259a753b5dae7ac2e2e
LMKgaming <leminh.khoa2907@gmail.com>
2026-05-14 00:48:48 +0700

    Fix: Animated CustomNavBar splash problem

--- src/frontend/components/CustomNavBar.tsx
+++ src/frontend/components/CustomNavBar.tsx
@@ -1,26 +1,68 @@
-import { View, TouchableOpacity, StyleSheet } from "react-native";
-import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
-import Animated, { FadeIn, FadeOut, LinearTransition } from "react-native-reanimated";
 import { AntDesign, Feather } from "@expo/vector-icons";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { useEffect } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import Animated, { interpolateColor, useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
 
 const PRIMARY_COLOR = "#ECFEFF";
 const SECONDARY_COLOR = "#0891B2";
+const BASE_SIZE = 10;
+
+function getIconByRouteName(routeName: string, color: string) {
+    switch (routeName) {
+        case "home":
+            return <Feather name="home" size={18} color={color} />;
+        case "history":
+            return <AntDesign name="bar-chart" size={18} color={color} />;
+        case "notification":
+            return <Feather name="bell" size={18} color={color} />;
+        case "settings":
+            return <Feather name="settings" size={18} color={color} />;
+        default:
+            return <Feather name="home" size={18} color={color} />;
+    }
+}
+
+const TabItem = ({ route, isFocused, label, onPress }: any) => {
+    const progress = useSharedValue(isFocused ? 1 : 0);
+
+    useEffect(() => {
+        progress.value = withSpring(isFocused ? 1 : 0, { mass: (route.name.length * BASE_SIZE) / 10 });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isFocused]);
+
+    const animatedContainerStyle = useAnimatedStyle(() => {
+        return {
+            backgroundColor: interpolateColor(progress.value, [0, 1], ["transparent", SECONDARY_COLOR]),
+        };
+    });
+
+    const animatedMaskStyle = useAnimatedStyle(() => {
+        return {
+            maxWidth: progress.value * route.name.length * BASE_SIZE,
+            marginLeft: progress.value * 8,
+            opacity: progress.value,
+        };
+    });
+
+    return (
+        <AnimatedTouchableOpacity onPress={onPress} style={[styles.tabItem, animatedContainerStyle]}>
+            {getIconByRouteName(route.name, isFocused ? PRIMARY_COLOR : SECONDARY_COLOR)}
+            <Animated.Text numberOfLines={1} style={[styles.text, animatedMaskStyle]}>
+                {label}
+            </Animated.Text>
+        </AnimatedTouchableOpacity>
+    );
+};
 
 const CustomNavBar: React.FC<BottomTabBarProps> = ({ state, descriptors, navigation }) => {
     const insets = useSafeAreaInsets();
 
     return (
-        <View
-            style={[
-                styles.container,
-                {
-                    bottom: Math.max(20, insets.bottom + 10),
-                },
-            ]}
-        >
+        <View style={[styles.container, { bottom: Math.max(20, insets.bottom + 10) }]}>
             {state.routes.map((route, index) => {
                 if (["_sitemap", "+not-found"].includes(route.name)) return null;
 
@@ -47,42 +89,17 @@ const CustomNavBar: React.FC<BottomTabBarProps> = ({ state, descriptors, navigat
                 };
 
                 return (
-                    <AnimatedTouchableOpacity
-                        layout={LinearTransition.springify().mass(0.5)}
+                    <TabItem
                         key={route.key}
+                        route={route}
+                        isFocused={isFocused}
+                        label={label as string}
                         onPress={onPress}
-                        style={[styles.tabItem, { backgroundColor: isFocused ? SECONDARY_COLOR : "transparent" }]}
-                    >
-                        {getIconByRouteName(route.name, isFocused ? PRIMARY_COLOR : SECONDARY_COLOR)}
-                        {isFocused && (
-                            <Animated.Text
-                                entering={FadeIn.duration(200)}
-                                exiting={FadeOut.duration(200)}
-                                style={styles.text}
-                            >
-                                {label as string}
-                            </Animated.Text>
-                        )}
-                    </AnimatedTouchableOpacity>
+                    />
                 );
             })}
         </View>
     );
-
-    function getIconByRouteName(routeName: string, color: string) {
-        switch (routeName) {
-            case "home":
-                return <Feather name="home" size={18} color={color} />;
-            case "history":
-                return <AntDesign name="bar-chart" size={18} color={color} />;
-            case "notification":
-                return <Feather name="bell" size={18} color={color} />;
-            case "settings":
-                return <Feather name="settings" size={18} color={color} />;
-            default:
-                return <Feather name="home" size={18} color={color} />;
-        }
-    }
 };
 
 const styles = StyleSheet.create({
@@ -94,13 +111,14 @@ const styles = StyleSheet.create({
         backgroundColor: PRIMARY_COLOR,
         width: "70%",
         alignSelf: "center",
-        borderRadius: 40,
+        borderRadius: 36 + 15 * 2,
         paddingHorizontal: 12,
         paddingVertical: 15,
-        shadowColor: PRIMARY_COLOR,
-        shadowOffset: { width: 0, height: 5 },
-        shadowOpacity: 0.3,
-        shadowRadius: 5,
+        shadowColor: "#333333" + "CC",
+        shadowOffset: { width: 0, height: 10 },
+        shadowOpacity: 0.12,
+        shadowRadius: 18,
+        elevation: 6,
     },
     tabItem: {
         flexDirection: "row",
